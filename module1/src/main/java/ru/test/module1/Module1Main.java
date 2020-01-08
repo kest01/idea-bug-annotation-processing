762e710d624110cb7d473cc532ec85507e86a7f6
Konstantin Kharitonov <kkharitonov@luxoft.com>
2020-01-08 18:00:30 +0300

    initial commit

---
+++ module1/src/main/java/ru/test/module1/Module1Main.java
@@ -0,0 +1,8 @@
+package ru.test.module1;
+
+public class Module1Main {
+
+    public static void main(String[] args) {
+        System.out.println("Module1");
+    }
+}
---
+++ module2/src/main/java/ru/test/module2/Module2Main.java
@@ -0,0 +1,8 @@
+package ru.test.module2;
+
+public class Module2Main {
+
+    public static void main(String[] args) {
+        System.out.println("Module2");
+    }
+}
